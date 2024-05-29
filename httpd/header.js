7e3b3d0160fc3c6c3b3b2be3c8d2b4ecb309f355
Jackson Brienen <brienenj@cwu.edu>
2024-05-29 06:32:19 -0700

    Added navigation bar

--- httpd/header.js
+++ httpd/header.js
@@ -4,15 +4,25 @@
  Description: Dynamically loads the header for all pages, this is the only place the header should be modified
  */
 
-const headerHTML =
+function headerHTML() {
+    const header = document.createElement('header');
+    header.innerHTML = headerInnerHTML;
+    return header;
+}
+
+const headerInnerHTML =
     `
-     <header>
-        <img src="icon.png">
-        <h1>CS Secure</h1>
-        <h4>We're Securing your Future</h4>
-     </header>
+    <img src="icon.png">
+    <h1>CS Secure</h1>
+    <h4>We're Securing your Future</h4>
+    <nav>
+        <a href="/"><img class="home nav-button"></a>
+        <a href="/welcome"><img class="login nav-button"></a>
+        <a href="/manager"><img class="account nav-button"></a>
+    </nav>
     `;
 
 window.addEventListener('load', ()=> {
-    document.body.innerHTML = headerHTML + document.body.innerHTML;
+    const header = headerHTML();
+    document.body.appendChild(header);
 });
